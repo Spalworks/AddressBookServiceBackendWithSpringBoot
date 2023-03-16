18f2fed91929d6226afa2977fab7c12bdc6fa31f
Sourav Pal <spalworks@gmail.com>
2023-03-16 09:48:15 +0530

    Added: Email Sending Service

--- src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controller/AddressBookController.java
@@ -28,6 +28,8 @@ public class AddressBookController {
 	@Autowired
 	private IAddressBookService service;
 
+	
+	
 	@GetMapping(value = { "", "/", "/getAll" })
 	public ResponseEntity<ResponseDTO> getAddressbookData() {
 		List<User> result = service.getAllUser();
@@ -35,6 +37,7 @@ public class AddressBookController {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.FOUND);
 	}
 
+	
 	@GetMapping(value = "/getbyid/{userId}")
 	public ResponseEntity<ResponseDTO> getUserById(@PathVariable int userId) {
 		User result = service.getUserById(userId);
@@ -42,13 +45,15 @@ public class AddressBookController {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.FOUND);
 	}
 
+	
 	@GetMapping(value = "/getbyemail/{email}")
 	public ResponseEntity<ResponseDTO> getUserByMail(@PathVariable String email) {
 		User result = service.getUserByEmail(email);
 		ResponseDTO responseDTO = new ResponseDTO(result, "Data retrived Successfully");
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.FOUND);
 	}
-
+ 
+	
 	@PostMapping(value = "/createuser")
 	public ResponseEntity<ResponseDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
 		User result = service.createNewUser(userDTO);
@@ -56,6 +61,7 @@ public class AddressBookController {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.CREATED);
 	}
 
+	
 	@PutMapping(value = "/updateuser/{userId}")
 	public ResponseEntity<ResponseDTO> updateUserById(@PathVariable("userId") int id,
 			@Valid @RequestBody UserDTO userDTO) {
@@ -65,12 +71,14 @@ public class AddressBookController {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 
+	
 	@DeleteMapping(value = "/deleteuser/{id}")
 	public ResponseEntity<ResponseDTO> deleteUserById(@PathVariable int id) {
 		service.deleteUserById(id);
 		ResponseDTO responseDTO = new ResponseDTO("Deleted Data Successfully", "Deleted id: " + id);
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
+
 	
 	@PostMapping(value = "/create-user-by-token")
 	public ResponseEntity<ResponseDTO> createUserByToken(@Valid @RequestBody UserDTO userDTO) {
@@ -78,6 +86,7 @@ public class AddressBookController {
 		ResponseDTO responseDTO = new ResponseDTO(token, "Userdata created Successfully with JWT token");
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.CREATED);
 	}
+	
 	
 	@PutMapping(value = "/update-user-by-token/{token}")
 	public ResponseEntity<ResponseDTO> updateUserByToken(@PathVariable String token,
@@ -88,12 +97,14 @@ public class AddressBookController {
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.OK);
 	}
 	
+	
 	@GetMapping(value = "/get-user-by-token/{token}")
 	public ResponseEntity<ResponseDTO> getUserByToken(@PathVariable String token) {
 		User result = service.getUserDataByToken(token);
 		ResponseDTO responseDTO = new ResponseDTO(result, "Data retrived Successfully using token");
 		return new ResponseEntity<ResponseDTO>(responseDTO, HttpStatus.FOUND);
 	}
+	
 	
 	@DeleteMapping(value = "/delete-user-by-token/{token}")
 	public ResponseEntity<ResponseDTO> deleteUserByToken(@PathVariable String token) {
